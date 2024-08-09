78b8fb3043069328d45a0b890fd3f4bb307558b7
Kgomotso Leonard Nkosi <futurekgomotso@gmail.com>
2024-08-09 13:56:32 +0200

    link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,25 +5,25 @@ const Footer = () => {
     <>
 <div className="mt-8 w-full bg-[#0C1844] px-8 md:px-[300px] flex md:flex-row flex-col space-y-6 md:space-y-0 items-start md:justify-between text-sm md:text-md py-8 ">
        <div className="flex flex-col text-white">
-         <p>Featured Blogs</p>
-         <p>Most viewed</p>
-         <p>Readers Choice</p>
+         <a href="#">Featured Blogs</a>
+         <a href="#">Most viewed</a >
+         <a href="#">Readers Choice</a>
        </div>
 
        <div className="flex flex-col text-white">
-         <p>Forum</p>
-         <p>Support</p>
-         <p>Recent Posts</p>
+         <a href="#">Forum</a >
+         <a href="#">Support</a>
+         <a href="#">Recent Posts</a>
        </div>
 
        <div className="flex flex-col text-white">
-         <p>Privacy Policy</p>
-         <p>About Us</p>
-         <p>Terms & Conditions</p>
-         <p>Terms of Service</p>
+         <a href="#">Privacy Policy</a>
+         <a href="#">About Us</a >
+         <a href="#">Terms & Conditions</a>
+         <a href="#">Terms of Service</a>
        </div>
     </div>
-    <p className="py-2 pb-6 text-center text-white bg-[#C80036] text-sm">All rights reserved @BLOGiq 2023</p>
+    <p className="py-2 pb-6 text-center text-white bg-[#C80036] text-sm">All rights reserved | @BLOGiq {new Date().getFullYear()}</p>
     </>
     
   )
