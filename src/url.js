bb9be25293e84d7f30997aa2a80cfd470e350df8
Kgomotso Leonard Nkosi <futurekgomotso@gmail.com>
2024-08-09 08:24:41 +0200

    trt

--- src/url.js
+++ src/url.js
@@ -1,5 +1,5 @@
-// export const URL=import.meta.env.VITE_URL
-// export const IF=import.meta.env.VITE_IF
+export const URL=import.meta.env.VITE_URL
+export const IF=import.meta.env.VITE_IF
 
-export const URL='https://backend-2uq6.onrender.com'
-export const IF="https://backend-2uq6.onrender.com/images/"
+// export const URL='https://backend-2uq6.onrender.com'
+// export const IF="https://backend-2uq6.onrender.com/images/"
